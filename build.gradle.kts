b91ce429801dc72b84644bd289a42a0587fd2286
S1riyS <kirill.ankudinov.94@mail.ru>
2023-11-01 14:52:49 +0300

    fix(gradle): encoding changed to UTF-8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,27 @@
+val MAIN_CLASS = "org.example.Story"
+
 plugins {
     id("java")
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
-dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest.attributes["Main-Class"] = MAIN_CLASS
+    val dependencies = configurations.runtimeClasspath.get().map(::zipTree) // OR .map { zipTree(it) }
+    from(dependencies)
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
